d2b9928d95862f874c753ab9a2264f9b94344d1f
El <and10393@umn.edu>
2026-04-14 15:28:04 -0500

    Load File and Save to Variable
    Add dependencies to core.clj and project.clj.
    Move location of Health_AnimalBites.csv.
    Add initial code to load data and store as a variable.
    Add initial tests for loaded data.

--- animal-bites/test/animal_bites/core_test.clj
+++ animal-bites/test/animal_bites/core_test.clj
@@ -3,5 +3,11 @@
             [animal-bites.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Testing the loading of the datafile"
+    ;; file should exist
+    (is animal-data) ; loaded data should be stored in a real objec
+    (is (vector? animal-data)) ; clojure.lang.PersistentVector
+    ;; data should have [#] of columns (ie [#] of items in first array/row)
+    ;; data should have [#] rows
+    ;; data type should be strings
+    ))
