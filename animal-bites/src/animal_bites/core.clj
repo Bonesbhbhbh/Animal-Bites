670027df77e862675e33ec58b7a4484c6c5f8423
El <and10393@umn.edu>
2026-04-16 14:45:09 -0500

    Add three additional tests for checking validity of loaded data

--- animal-bites/src/animal_bites/core.clj
+++ animal-bites/src/animal_bites/core.clj
@@ -4,7 +4,7 @@
             [clojure.data.csv :as csv]))
 ;; This code is written by Orville Anderson (and later Harley Hannahs)
 
-(println "In core.clj of animal_bites")
+(println "In core.clj of animal_bites") ; quick check to see if running in correct file.
 (def animal-data 
   (with-open [reader (io/reader "Health_AnimalBites.csv")] ; this requires the data file to be stored at the project-level
     (let [data (csv/read-csv reader)]
--- animal-bites/test/animal_bites/core_test.clj
+++ animal-bites/test/animal_bites/core_test.clj
@@ -5,9 +5,9 @@
 (deftest a-test
   (testing "Testing the loading of the datafile"
     ;; file should exist
-    (is animal-data) ; loaded data should be stored in a real objec
+    (is animal-data) ; loaded data should be stored in a real object
     (is (vector? animal-data)) ; clojure.lang.PersistentVector
-    ;; data should have [#] of columns (ie [#] of items in first array/row)
-    ;; data should have [#] rows
-    ;; data type should be strings
+    (is (= 15 (count (first animal-data)))) ; first row(headers) should have 15 items
+    (is (= 9004 (count animal-data))) ; data should have [#] rows
+    (type (first (first animal-data))) ; first entry should be a string
     ))
