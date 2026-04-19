98298402d5f3ce853cf201901cdc4441bc3e7786
El <and10393@umn.edu>
2026-04-19 14:23:32 -0500

    Implement Column and add tests
    Function can take column out of a csv(as nested vec) when given data and a header. This function does not handle sparse data.

--- animal-bites/src/animal_bites/core.clj
+++ animal-bites/src/animal_bites/core.clj
@@ -9,3 +9,14 @@
   (with-open [reader (io/reader "Health_AnimalBites.csv")] ; this requires the data file to be stored at the project-level
     (let [data (csv/read-csv reader)]
       (reduce conj [] data))))
+
+(defn column
+  "Takes a csv formatted as a list of nested vectors and one string associated with a column header (value in first vector).
+  Returns all information in that column."
+  [data header] ; we may be able to use destructuring here to split off the first row of data
+    ;; should really check that the data is not empty and that the header is valid
+    ;; this function is written in a way where you could technically have two columns with same header, hence the `(first )`...
+    (first (for [i (range (count (first data))) ; for i in the number of headings:
+      :when (= header (nth (first data) i))] ; when the heading matches the heading passed to func
+      (map #(nth % i) data)))) ; take nth item from data
+      
